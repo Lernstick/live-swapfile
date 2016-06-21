067dfb31eb759444886a9a5962fae38adc11ef6f
Ronny Standtke <ronny.standtke@gmx.net>
2016-06-21 21:44:28 +0200

    updated gettext handling

--- update_mo.sh
+++ update_mo.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-msgfmt -o usr/share/locale/de/LC_MESSAGES/live-swapfile.mo po/de/live-swapfile.po
+cd po
+for PO_FILE in *.po; do
+	LANG_CODE="$(echo "${PO_FILE}" | sed 's/\.po$//1')"
+	LOCALE_DIR="../usr/share/locale/${LANG_CODE}/LC_MESSAGES/"
+	if [ ! -d "${LOCALE_DIR}" ]
+	then
+		mkdir -p "${LOCALE_DIR}"
+	fi
+	msgfmt "${PO_FILE}" -o "${LOCALE_DIR}/live-swapfile.mo"
+done
--- update_po.sh
+++ update_po.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 xgettext -L Shell --add-comments=## etc/init.d/live-swapfile usr/sbin/live-mkswapfile
-msgmerge -U po/de/live-swapfile.po messages.po
+for ${PO_FILE} in po/*.po; do
+  msgmerge -U "${PO_FILE}" messages.po
+done
 rm messages.po
